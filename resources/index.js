0d30f591452a112a0a5672de8c00f112384aa2b3
Sverre <sverreabo@gmail.com>
2021-05-09 13:49:30 +0200

    småfix

--- resources/index.js
+++ resources/index.js
@@ -1,9 +1,10 @@
 function main() {
     var d = new Date();
-    document.getElementById("currenttime").innerHTML = d.toUTCString();
+    current_time.innerHTML = d.toUTCString();
 
     setTimeout("main()", 500);
 
 }
 
+const current_time = document.getElementById("current_time");
 main();
--- resources/vigenere.js
+++ resources/vigenere.js
@@ -1,18 +1,21 @@
 async function get_url(url = '') {
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
+    try {
+        document.body.style.cursor = "wait";
+        const response = await fetch(url, {
+            method: 'GET', // *GET, POST, PUT, DELETE, etc.
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        });
+        return response.json();
+
+    } catch (err) {
+        alert("Kunne ikke koble til serveren. Sjekk IP-adressen.");
+    } finally {
+        document.body.style.cursor = "unset";
+    }
 }
 
 function encrypt() {
@@ -61,6 +64,7 @@ function brute_force() {
             }
 
             brute_time_field.innerHTML = String(total) + " sekunder"
+            document.body.style.cursor = "unset";
         });
 }
 
